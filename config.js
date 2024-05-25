7a3cbc8c9dfa7a049fd6693312b0bbbf539723fe
Sumedhalakshitha12 <sumedhalakshitha754@gmail.com>
2024-05-25 13:49:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,11 +21,11 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
-global.OwnerName = "SACHITH";
-global.BotName = "SACHITH";
+global.Owner = ["+94717835395"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+94717835395"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+94717835395"];
+global.OwnerName = "*𝛭𝑅.𝑆𝑈𝛭𝛪𝑌𝛥𝛨*";
+global.BotName = "*𝛭𝑅.𝑆𝑈𝛭𝛪𝑌𝛥𝛨*";
 global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
 global.author = "SACHITH";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
